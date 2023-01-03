1c14e17131d4ac08c649ef76b2219edac18c222a
felipemotavieira <felipemtvieira@gmail.com>
2023-01-03 13:26:10 -0300

    fix: app names

--- _core/settings.py
+++ _core/settings.py
@@ -30,14 +30,29 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    ]
+
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+    ]
+
+MY_APPS = [
+    'address',
+    'donees',
+    'institutions',
+    'solicitations',
+    'users',
+    ]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
