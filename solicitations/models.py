c2eade0f73322d451635be845cf24f5a77ac1957
liviabsl <liviasoliveira03@gmail.com>
2023-01-03 16:35:58 -0400

    feat:models

--- solicitations/models.py
+++ solicitations/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class StatusChoices(models.TextChoices):
+    DISPONIVEL = "Disponível"
+    NAO_DISPONIVEL = "Não disponível"
+    ENVIADO = "Enviado"
+    RECEBIDO = "Recebido"
+    NOT_INFORMED = "Não informado"
+
+class Solicitation(models.Model):
+    description = models.CharField(max_length=255)
+    status = models.CharField(max_length=20, choices= StatusChoices.choices, default= StatusChoices.NOT_INFORMED)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(
+        "user.User",
+        on_delete=models.CASCADE,
+        related_name="solicitations",
+    )
+
+#!!!!!!!!
+    donee = models.OneToOneField(
+        "donee.Donee",
+        on_delete=models.CASCADE,
+        related_name="solicitations",
+    )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    email = models.EmailField(max_length=255, unique=True)
+
+    address = models.OneToOneField(
+        "addresses.Address",
+        on_delete=models.CASCADE,
+        related_name="user",
+    )
