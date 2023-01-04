105d5785c5f8bca0a521e9d8a93c79cbfd00aee3
Leticia Angelim <leticia.angelim@hotmail.com>
2023-01-04 13:40:32 -0300

    fix: migrations

--- donees/models.py
+++ donees/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
+
 class Donee(models.Model):
     name = models.CharField(max_length=255)
-    age = models.IntegerField
+    age = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     institution = models.ForeignKey(
         "institutions.Institution",
         on_delete=models.CASCADE,
-        related_name="donees"
+        related_name="donees",
     )
