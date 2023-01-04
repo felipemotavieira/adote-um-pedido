a3433ca74101a965dcef5328bfd5bbc6e3dc4bf9
NaianeReis27 <email.annereis@gmail.com>
2023-01-04 17:54:41 -0300

    feat:atualizando o parametro

--- donees/permissions.py
+++ donees/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 class IsAdmin(permissions.BasePermission):
-       def has_permission(self, request):
+       def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return request.user.is_authenticated and request.user.is_superuser
   
--- donees/views.py
+++ donees/views.py
@@ -7,14 +7,14 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class doneeView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = IsAdmin
+    permission_classes = [IsAdmin]
     serializer_class = DoneeSerializers
     queryset = Donee.objects.all()
     
 
 class doneeDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = IsAdmin
+    permission_classes = [IsAdmin]
     serializer_class = DoneeSerializers
     queryset = Donee.objects.all()
     
