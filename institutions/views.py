ae014bca5856a82db5e43f9e99ea319129a47d20
Lucas Silva <lsengenheirocivil@gmail.com>
2023-01-04 20:19:18 -0300

    feat: create institution serializers-views

--- institutions/views.py
+++ institutions/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from .serializers import InstitutionSerializer
+from .models import Institution
+from rest_framework import generics
 
-# Create your views here.
+
+class InstitutionView(generics.ListCreateAPIView):
+    serializer_class = InstitutionSerializer
+    queryset = Institution.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class InstitutionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = InstitutionSerializer
+    queryset = Institution.objects.all()
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
