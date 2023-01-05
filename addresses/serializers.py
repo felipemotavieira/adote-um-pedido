15841b729509a87dde813312f5479daa1e326f38
Leticia Angelim <leticia.angelim@hotmail.com>
2023-01-05 13:24:17 -0300

    feat: address serializer

--- addresses/serializers.py
+++ addresses/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+
+from .exceptions import InstitutionDoesNotExist
+from institutions.models import Institution
 from .models import Address
 
 
@@ -6,15 +9,26 @@ class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
 
-        fields = '__all__'
+        fields = "__all__"
 
     def create(self, validated_data):
         created_address = Address.objects.create(**validated_data)
-        user = self.context['request'].user
-        
-        setattr(user, 'address', created_address)
+        user = self.context["request"].user
+
+        if user.is_staff is True and not user.is_superuser:
+            institution = Institution.objects.filter(owner=user)
+
+            if not institution:
+                raise InstitutionDoesNotExist
+
+            setattr(institution[0], "address", created_address)
+            user.save()
+
+            return created_address
+
+        setattr(user, "address", created_address)
         user.save()
-        
+
         return created_address
 
     def update(self, instance: Address, validated_data: dict):
