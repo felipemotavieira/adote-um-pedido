73209fc25bcca44962b612ede3e52de104d3d8d5
liviabsl <liviasoliveira03@gmail.com>
2023-01-10 17:11:24 -0400

    feat/testeApi

---
+++ apiPIX/credenciais.py
@@ -0,0 +1,6 @@
+CREDENTIALS = {
+    'client_id': 'Client_Id_7ee5211a238e1d7d9e79f247ec6a33e6ba20fa76',
+    'client_secret': 'Client_Secret_34d122329e4cd7b65bcb88b14d5522f18b59c9f7',
+    'sandbox': False,
+    'certificate': 'apiPIX/certificado.pem'
+}
