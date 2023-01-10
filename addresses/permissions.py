af2c902344e17e60e675d9a9aa0494c54d215844
felipemotavieira <felipemtvieira@gmail.com>
2023-01-10 16:20:58 -0300

    fixed address

--- addresses/permissions.py
+++ addresses/permissions.py
@@ -4,14 +4,19 @@ from .models import Address
 from institutions.models import Institution
 import ipdb
 
+class IsAuthenticated(permissions.BasePermission):
+    def has_permission(self, request: Request, view: View):
+        return request.user.is_authenticated
+
+
 class IsUserOrInstitutionAddress(permissions.BasePermission):
     def has_object_permission(self, request: Request, view: View, obj: Address):
-        institution = Institution.objects.get(address_id = view.kwargs["address_id"])
+        institution = Institution.objects.filter(address_id = view.kwargs["address_id"])
 
         if institution:
             return True
 
-        if request.user.address_id == obj.id:
+        if request.user.is_authenticated and request.user.address_id == obj.id:
             return True
 
         return request.user.is_superuser
