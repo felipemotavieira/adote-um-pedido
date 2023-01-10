fde226ddfd91d4d91a18b0637dacb3c6e3985277
Galvs135 <lg_franca@hotmail.com>
2023-01-10 17:20:57 -0300

    feat: send-Email

--- _core/settings.py
+++ _core/settings.py
@@ -13,11 +13,18 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from django.core.management.utils import get_random_secret_key
+import environ
 import os
 import dj_database_url
 import dotenv
 
 dotenv.load_dotenv()
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -182,3 +189,12 @@ SPECTACULAR_SETTINGS = {
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
 }
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env('EMAIL_PORT')
