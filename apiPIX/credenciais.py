34ac691c418758f584cdbda27c093493272f9db3
Lucas Silva <lsengenheirocivil@gmail.com>
2023-01-11 21:17:34 -0300

    feat: env

--- apiPIX/credenciais.py
+++ apiPIX/credenciais.py
@@ -1,6 +1,12 @@
+import dotenv
+import os
+
+
+dotenv.load_dotenv()
+
 CREDENTIALS = {
-    'client_id': 'Client_Id_7ee5211a238e1d7d9e79f247ec6a33e6ba20fa76',
-    'client_secret': 'Client_Secret_34d122329e4cd7b65bcb88b14d5522f18b59c9f7',
+    'client_id': os.getenv("CLIENT_ID"),
+    'client_secret': os.getenv("CLIENT_SECRET"),
     'sandbox': False,
     'certificate': 'apiPIX/certificado.pem'
 }
